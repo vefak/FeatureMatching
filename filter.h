710c194958ee09d051f569fff6a581b53ec9795e
vefak <vmakman@gmail.com>
2020-08-02 02:31:52 +0300

    add filter.h and filter.cpp files

---
+++ filter.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+using namespace cv;
+using namespace cv::xfeatures2d;
+using namespace std;
+
+
+vector<Point2f> findingPlanar(Mat img_scene, Mat img_object, bool showImages, int hessian, float ratio);
+void displayCorners(string namecorner, vector<Point2f> corners);
+
