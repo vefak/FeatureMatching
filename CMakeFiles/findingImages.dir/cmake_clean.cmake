aa3db73166536dca4e26e15daf1a31aa77733447
vefak <vmakman@gmail.com>
2020-08-01 23:33:07 +0300

    cmake files and run file

---
+++ CMakeFiles/findingImages.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/findingImages.dir/main.cpp.o"
+  "findingImages.pdb"
+  "findingImages"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/findingImages.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
