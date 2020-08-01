d5a50a5d1f721bb9b2df42dbd945385f569d7488
vefak <vmakman@gmail.com>
2020-08-02 00:00:33 +0300

    finish finding small_area

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "opencv2/core.hpp"
-#ifdef HAVE_OPENCV_XFEATURES2D
 #include "opencv2/calib3d.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
@@ -11,13 +10,14 @@ using namespace cv::xfeatures2d;
 using namespace std;
 
 bool showImages = false;
-int main( int argc, char* argv[] )
-{
-    Mat img_object = imread("./img/Small_area_rotated.png", CV_LOAD_IMAGE_GRAYSCALE);
-    //Mat img_scene = imread( "./img/StarMap.png" , CV_LOAD_IMAGE_GRAYSCALE );
 
-    Mat img_object = imread("./img/box.png", CV_LOAD_IMAGE_GRAYSCALE);
-    Mat img_scene = imread( "./img/box_in_scene.png" , CV_LOAD_IMAGE_GRAYSCALE );
+int main( )
+{
+    Mat img_object = imread("./img/Small_area.png", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat img_scene = imread( "./img/StarMap.png" , CV_LOAD_IMAGE_GRAYSCALE );
+
+    //Mat img_object = imread("./img/box.png", CV_LOAD_IMAGE_GRAYSCALE);
+    //Mat img_scene = imread( "./img/box_in_scene.png" , CV_LOAD_IMAGE_GRAYSCALE );
 
     if ( img_object.empty() || img_scene.empty() )
     {
@@ -42,19 +42,22 @@ int main( int argc, char* argv[] )
     
 
     //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-    int minHessian = 50;
+    int minHessian = 10;
     Ptr<SURF> detector = SURF::create( minHessian );
     std::vector<KeyPoint> keypoints_object, keypoints_scene;
     Mat descriptors_object, descriptors_scene;
     detector->detectAndCompute( img_object, noArray(), keypoints_object, descriptors_object );
     detector->detectAndCompute( img_scene, noArray(), keypoints_scene, descriptors_scene );
+    
+    
+  
     //-- Step 2: Matching descriptor vectors with a FLANN based matcher
     // Since SURF is a floating-point descriptor NORM_L2 is used
     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
     std::vector< std::vector<DMatch> > knn_matches;
     matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
     //-- Filter matches using the Lowe's ratio test
-    const float ratio_thresh = 0.55f;
+    const float ratio_thresh = 0.50f;
     std::vector<DMatch> good_matches;
     for (size_t i = 0; i < knn_matches.size(); i++)
     {
@@ -63,6 +66,8 @@ int main( int argc, char* argv[] )
             good_matches.push_back(knn_matches[i][0]);
         }
     }
+    cout << good_matches.size() << endl;
+   
     //-- Draw matches
     Mat img_matches;
     drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
@@ -77,7 +82,6 @@ int main( int argc, char* argv[] )
         //-- Get the keypoints from the good matches
         obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
         scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
-        cout <<   keypoints_object[ good_matches[i].queryIdx ].pt << endl;  
     }
     Mat H = findHomography( obj, scene, RANSAC );
     //-- Get the corners from the image_1 ( the object to be "detected" )
@@ -104,10 +108,4 @@ int main( int argc, char* argv[] )
     waitKey();
     return 0;
 }
-#else
-int main()
-{
-    std::cout << "This tutorial code needs the xfeatures2d contrib module to be run." << std::endl;
-    return 0;
-}
-#endif
+
